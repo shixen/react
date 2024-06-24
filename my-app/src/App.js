1dc9ab4324d33f0015f435778c4a6102fc7cca99
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-06-24 19:42:42 +0000

    add exit button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <StatefulGreeting greeting="i'm a stateful class component!"
+       name="Niklas"/>
     </div>
   );
 }

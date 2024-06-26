1707148516ef45a343b8f76ac161148759bc0a3b
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-06-26 18:47:04 +0000

    state learning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,11 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWIthPrevState';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <StatefulGreetingWithCallback greeting="i'm a stateful class component!"
-       name="Niklas"/>
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreetingWIthPrevState.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+class StatefulGreetingWithPrevState extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!!!",
+            buttonText: "Exit",
+            count: 0
+        };
+    }
+
+    handleClick() {
+        this.setState(
+          {
+            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+          },
+          () => {
+            console.log("new state", this.state.introduction);
+            console.log("new state", this.state.buttonText);
+          }
+        );
+      }
+
+      increment(){
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log(this.state.count)
+      }
+    
+    render() {
+        return(
+            <div>
+                <h1> {this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.incrementFive()}>increment five </button>
+                <p> you have clicked the button {this.state.count} times</p>
+            </div>
+        );
+    }
+}
+
+export default StatefulGreetingWithPrevState;
